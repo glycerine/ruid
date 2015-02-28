90df7571c6351ea19558020631ff6143e35f8324
Jason E. Aten <j.e.aten@gmail.com>
2015-02-27 16:14:26 -0800

    use improved external IP address detection

--- cmd/ruid2gen/ruid2gen.go
+++ cmd/ruid2gen/ruid2gen.go
@@ -2,44 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/glycerine/ruid"
-	"net"
-	"regexp"
 )
 
 // generate Ruid2 unique id.
 
 func main() {
-	myExternalIP := GetExternalIP()
+	myExternalIP := ruid.GetExternalIP()
 	ruidGen := ruid.NewRuidGen(myExternalIP)
 	fmt.Printf("%s\n", string(ruidGen.Ruid2()))
 }
-
-func GetExternalIP() string {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		panic(err)
-	}
-
-	valid := []string{}
-
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			addr := ipnet.IP.String()
-			match := validIPv4addr.FindStringSubmatch(addr)
-			if match != nil {
-				if addr != "127.0.0.1" {
-					valid = append(valid, addr)
-				}
-			}
-		}
-	}
-	switch len(valid) {
-	case 0:
-		return "127.0.0.1"
-	default:
-		return valid[0]
-	}
-}
-
-var validIPv4addr = regexp.MustCompile(`^[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+$`)
