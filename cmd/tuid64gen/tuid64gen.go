57b7631b6a95ea29d641f621f513f9bb97039a78
Jason E. Aten <j.e.aten@gmail.com>
2015-09-20 13:46:56 -0700

    smaller tuid64, include tuid64gen

---
+++ cmd/tuid64gen/tuid64gen.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/glycerine/ruid"
+)
+
+func main() {
+	myExternalIP := ruid.GetExternalIP()
+	ruidGen := ruid.NewRuidGen(myExternalIP)
+	fmt.Printf("%s\n", string(ruidGen.Tuid64()))
+}
